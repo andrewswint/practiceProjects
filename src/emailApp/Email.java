02ed19dfd0cba858b0de9d9990d7e84c700facb1
Andrew Swint <andrewswint1976@gmail.com>
2024-02-12 13:02:59 -0800

    creating email app

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -2,4 +2,29 @@ package emailApp;
 
 public class Email {
 
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String departmant;
+	private int mailboxCapacity;
+	private String alternateEmail;
+
+//	Constructor to receive the first name and last name
+
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email created for " + firstName + " " + lastName);
+	}
+
+//	Ask for the department
+
+//	Generate a random password
+
+//	Set the mailbox capacity
+
+//	Set alternate email
+
+//	Change password
+
 }
