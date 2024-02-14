cb9c35bd29640f4e34b0db80121eb427a409d105
Andrew Swint <andrewswint1976@gmail.com>
2024-02-14 15:35:26 -0800

    course enrollment

--- src/studentDatabaseApp/Student.java
+++ src/studentDatabaseApp/Student.java
@@ -8,8 +8,8 @@ public class Student {
 	private String lastName;
 	private int gradeYear;
 	private String studentId;
-	private String courses;
-	private int tuitionBalance;
+	private String courses = "";
+	private int tuitionBalance = 0;
 	private static int costOfCourse = 500;
 	private static int id = 1000;
 
@@ -25,7 +25,7 @@ public class Student {
 		System.out.println("1-Freshmen\n2-Sophmore\n3-Junior\n4-Senior\nEnter student year: ");
 		this.gradeYear = sc.nextInt();
 
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		System.out.println(firstName + " " + lastName + " " + "is in " + gradeYear);
 
 		setStudentId();
 		System.out.println("Student ID is: " + studentId);
@@ -38,6 +38,21 @@ public class Student {
 	}
 
 //	Enroll in courses
+	public void enroll() {
+		do {
+			System.out.println("Enter crouse to enroll (Q to quit): ");
+			Scanner sc = new Scanner(System.in);
+			String course = sc.nextLine();
+			if (!course.equalsIgnoreCase("Q")) {
+				this.courses = courses + "\n" + course;
+				this.tuitionBalance = tuitionBalance + costOfCourse;
+			} else {
+				break;
+			}
+		} while (true);
+		System.out.println("Enrolled in " + courses);
+		System.out.println("Tuition balance is " + tuitionBalance);
+	}
 
 //	View balance
 
